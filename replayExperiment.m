51bc48a10e1ea9ea0bc3c8cd2bdc32e55659a811
Roger  Herikstad <grogee@work21.neuralc>
2013-08-10 13:18:27 +0800

    Added function to replay the experiment. Still work in progress

---
+++ replayExperiment.m
@@ -0,0 +1,76 @@
+function ans = replayExperiment(offset,nsamples,edfdata,l)
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	%Replay the experiment from the eye link data contained in the edfdata structure.
+	%Inputs:
+	%	offset		:		time point index from which to start the replay
+	%	nsamples	:		number of time points to replay
+	%	edfdata		:		eye link data structure contraining the experiment
+	%	l			:		reference to a line plot handle
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    if nargin == 3
+        figure
+        axis
+        l = plot(edfdata.FSAMPLE.gx(1,1),edfdata.FSAMPLE.gy(1,1),'.');
+        rectangle('Position', [144,90,1440-2*144,900-2*90]);
+        xdiff = (1440-2*144)/5;
+        ydiff = (900-2*90)/5;
+        for j=0:4
+            line([144, 1440-144], [90+j*ydiff 90+j*ydiff],'Color','k');
+        end
+        for i=0:4
+            line([144+i*xdiff, 144+i*xdiff], [90 900-90],'Color','k');
+        end
+        hold on
+        fill([144+2*xdiff, 144+3*xdiff, 144+3*xdiff, 144+2*xdiff],...
+            [90+3*ydiff, 90+3*ydiff, 90+2*ydiff, 90+2*ydiff],[0.5,0.5,0.5]);
+        xlim([0,1440]);
+        ylim([0,900]);
+    end
+    lifetime = -1;
+    nextevent = 1;
+    while edfdata.FEVENT(nextevent).sttime < edfdata.FSAMPLE.time(offset)
+        nextevent = nextevent + 1;
+    end
+    for i=1:nsamples,
+        set(l,'XData',edfdata.FSAMPLE.gx(1,offset+i),'YData',edfdata.FSAMPLE.gy(1,offset+i));
+        pause(0.01);
+        %check if there is an event at this time
+        if edfdata.FEVENT(nextevent).sttime == edfdata.FSAMPLE.time(offset+i)
+            m = edfdata.FEVENT(nextevent).message(1:3:end);
+            if ~isempty(m)
+                if ((m(1) == '0') && (m(2) == '1'))
+                    px = bin2dec(m(5:-1:3))-1;
+                    py = bin2dec(m(8:-1:6))-1;
+                    lifetime = 0;
+                end
+            end
+            nextevent = nextevent + 1;
+                
+        end
+        
+        %if lifetime == -1
+        %    k = rand;
+        %else
+        %    k = 1;
+        %end
+        %if k < 0.1
+        %    px = randi(5)-1;
+        %    py = randi(5)-1;
+        %    if (px ~= 2) && (py ~= 2)
+        if lifetime > -1
+            h = fill([144+px*xdiff, 144+(px+1)*xdiff, 144+(px+1)*xdiff, 144+px*xdiff],...
+            [90+(py+1)*ydiff, 90+(py+1)*ydiff, 90+py*ydiff, 90+py*ydiff],'r');
+                    %lifetime = 0;
+                %end
+        end
+        if lifetime>-1
+            lifetime = lifetime + 1;
+        end
+        if lifetime >= 50
+            delete(h);
+            lifetime = -1;
+        end
+        title(sprintf('%d', edfdata.FSAMPLE.time(offset+i)))
+        drawnow
+    end
+end
