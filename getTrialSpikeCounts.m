de4100ed12a046fb6a243f67ca7e63ea5655d609
Roger Herikstad <roger.herikstad@gmail.com>
2013-08-27 15:57:42 +0800

    Fixed a bug where aligning to target would cause an error

--- getTrialSpikeCounts.m
+++ getTrialSpikeCounts.m
@@ -12,10 +12,10 @@ function [counts, bins] = getTrialSpikeCounts(sptrain,trials,bins, alignment_eve
 	%								start of fixation
 	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	if nargin == 3
-		alignment_event = prestim
+		alignment_event = 'prestim';
 	end
 	if ~isfield(trials, alignment_event)
-		alignment_event = prestim
+		alignment_event = 'prestim';
 	end
 	ntrials = length(trials);
 	counts = zeros(ntrials,length(bins));
@@ -24,7 +24,11 @@ function [counts, bins] = getTrialSpikeCounts(sptrain,trials,bins, alignment_eve
     for t=1:ntrials
         if ~isempty(trials(t).start) && ~isempty(trials(t).end)
             idx = (spiketimes >= (trials(t).start*1000))&(spiketimes<=(trials(t).end*1000));
-            spikes = (spiketimes(idx)'-trials(t).start*1000 - getfield(trials(t),alignment_event)*1000);
+            alignto = getfield(trials(t),alignment_event);
+            if isstruct(alignto)
+                alignto = alignto.timestamp;
+            end
+            spikes = (spiketimes(idx)'-trials(t).start*1000 - alignto*1000);
 			c = histc(spikes,bins);
 			counts(t,:) = c;
         end
