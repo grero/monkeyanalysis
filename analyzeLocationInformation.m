13802bb698896929b51ffc43b9df83be70f32177
Roger Herikstad <roger.herikstad@gmail.com>
2013-08-28 07:49:25 +0800

    Added doc string to analyzeLocationInformation

--- analyzeLocationInformation.m
+++ analyzeLocationInformation.m
@@ -1,5 +1,13 @@
 function analyzeLocationInformation(sptrains,trials,bins,alignment_event)
-	
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	%Compute and plot the information for several spike trains. The plots
+	%are saved under the current directory as gXXcXXsLocationInformation.pdf
+	%Input:
+	%	sptrains		:		structure array of spike strains
+	%	trials			:		structure array of trial information
+	%	bins			:		the bins into which the spike trains should be discretized
+	%	alignment_event	:		the event to which to align the spike trains
+	%
 	for ch=1:length(sptrains.spikechannels)
 		clusters = sptrains.channels(sptrains.spikechannels(ch)).cluster;
 		for j=1:length(clusters)
