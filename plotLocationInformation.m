9a9cb449ddfe1429a4bd6e2934d3ceb86b0c7c04
Roger Herikstad <roger.herikstad@gmail.com>
2013-08-28 07:45:33 +0800

    Added optional plotting of shuffled information

--- plotLocationInformation.m
+++ plotLocationInformation.m
@@ -1,4 +1,4 @@
-function plotLocationInformation(I,bins,alignment_event,trials)
+function plotLocationInformation(I,bins,alignment_event,trials,Is)
 	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	%Plot raster for different target locations
 	%Input:
@@ -8,7 +8,11 @@ function plotLocationInformation(I,bins,alignment_event,trials)
 	%	alignment_event	:	the event to which the spike counts were 
 	%						aligned
 	%	trials			:	structure array of trials information
+%	%	Is				:	[optional] shuffle information obtained by shuffle trial labels
 	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	if nargin  == 4
+		Is = [];
+	end
 	response = zeros(length(trials),1);
 	for t=1:length(trials)
 		response(t) = trials(t).response;
@@ -29,7 +33,15 @@ function plotLocationInformation(I,bins,alignment_event,trials)
 	h1 = plot([R,R], [yl(1),yl(2)],'r','linewidth',2);
 	h2 = plot([rl,rl], [yl(1),yl(2)],'r');
 	h3 = plot([rh,rh], [yl(1),yl(2)],'r');
-	legend(h1,'Median response period onset','Location','NorthWest');
+	if ~isempty(Is)
+		%plot the lower bound as mean + 3 std devations of the shuffled information
+		M = mean(Is,1);
+		S = std(Is,1);
+		h4 = plot(bins,M+3*S,'g');
+		legend([h1,h4],'Median response period onset','Shuffle info [mean + 3td]','Location','NorthWest');
+	else
+		legend(h1,'Median response period onset','Location','NorthWest');
+	end
 	%prettify plot
 	set(gca,'Box','Off');
 	set(gca, 'TickDir','out');
