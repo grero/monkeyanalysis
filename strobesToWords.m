99550bdc8df61529d89168173dd50d30f3e235fa
Roger Herikstad <roger.herikstad@gmail.com>
2013-09-05 17:24:30 +0800

    Added more decoding approaches to strobesToWords

--- strobesToWords.m
+++ strobesToWords.m
@@ -1,11 +1,24 @@
 function words = strobesToWords(strobes)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Convert int16 strobes to binary words
+%Input:
+%	strobes		:	strobes encoded as 16 bit integers
+%Output:
+%	words		:	matrix of doubles were each row is the binary represntation
+%					of a strobe word
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-	
-	words = dec2bin(2^15-abs(strobes));
-	%get the last 8 bits
-	words = words(:,end-7:end)-'0';
+	%check the value of the first strobe to get the decoding
+	if strobes(1) == 4415
+		%we are using an inverted scheme
+		words = ~logical(dec2bin(strobes)-'0');
+		words = double(words(:,end-7:end));
+	elseif strobes(1) == -4416
+		%this is the old scheme
+		words = dec2bin(2^15-abs(strobes)) - '0';
+		words = words(:,end-7:end);
+	else
+		disp('Unknown strobe encoding');
+	end
 end
 	 
 	
