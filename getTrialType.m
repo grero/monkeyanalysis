659aeef81d471fe486b748657c86c68146449b54
Roger  Herikstad <roger.herikstad@gmail.com>
2013-09-18 09:45:28 +0800

    Updated getTrialType.m from devel

--- getTrialType.m
+++ getTrialType.m
@@ -1,19 +1,31 @@
-function rtrials  = getTrialType( trials,type )
-%Return a subset of the trials of the specified type. The type should
-%correspond to a field in the trial structure e.g. 
-%getTrialType(trials,'target') will return all trials in which a
-%target appeared. 
-%Input:
-%   trials          :    trial structure obtained from loadTrialInfo
-%   type            :    string indicating which trial type to get.
-%                        Possible values are: 'target' and'failure'. 
+function rtrials  = getTrialType( trials,varargin )
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	%Return a subset of the trials of the specified type. The type should
+	%correspond to a field in the trial structure e.g. 
+	%getTrialType(trials,'target') will return all trials in which a
+	%target appeared. 
+	%Input:
+	%   trials          :    trial structure obtained from loadTrialInfo
+	%   varargin        :    strings indicating which trial type to get.
+	%   					 any combination of fields found in the trials
+	%   					 structure can be specified
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     k = 1;
-    
+	types = varargin;
+	ntypes = length(types);
     for t=1:length(trials)
-        if ~isempty(getfield(trials(t),type))
+		include = 1;
+		i = 1;
+		while include && (i <= ntypes) 
+			if isempty(getfield(trials(t),types{i}))
+				include = 0;
+			end
+			i = i + 1;
+		end
+		if include == 1
             rtrials(k) = trials(t);
             k = k+1;
-        end
+		end
     end
 end
 
