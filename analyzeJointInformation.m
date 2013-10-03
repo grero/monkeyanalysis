8c7a34408d06e5acf21fa84f74e0795dd9dd0060
Roger Herikstad <roger.herikstad@gmail.com>
2013-10-03 14:56:34 +0800

    Added modified functions to compute joint information

---
+++ analyzeJointInformation.m
@@ -0,0 +1,181 @@
+function [Z,Zs,Zi,onsets,offsets] = analyzeJointInformation(sptrains,trials,bins,alignment_event,sort_event,doplot,dosave,group1,group2,logfile)
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	%Compute and plot the paired information for several spike trains. The plots
+	%are saved under the current directory as gXXcXXsJointInformation.pdf
+	%Input:
+	%	sptrains		:		structure array of spike strains
+	%	trials			:		structure array of trial information
+	%	bins			:		the bins into which the spike trains should be discretized
+	%	alignment_event	:		the event to which to align the spike trains
+    %   sort_event      :       the event used to sort the trials
+    %   doplot          :       specifies whether to plot (doplot = 1) or
+    %                            not (doplot=0). The default is to plot
+    %   group1          :       the first group of cells
+    %   group2          :       the second group of cells
+    %Output:
+    %   Z               :       ncells X ncells X nbins matrix of information
+    %                           transfer values. The element (i,j,k) of this matrix 
+    %                           quantifies how much information encoded jointly 
+    %                           by cell j and cell i at time bin k
+	%	Zs				:		shuffled version of Z, obtained by shuffling 
+	%							trial labels
+	%	Zi				:		independent version of Z, obtained by adding the 
+	%							information encoded by each cell separately
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    if nargin < 9
+        logfile = 1;
+    end
+    if nargin < 8
+        group1 = sptrains.spikechannels;
+    else
+        %make sure we are using valid channels
+        group1 = intersect(group1,sptrains.spikechannels);
+    end
+    if nargin < 9
+        group2 = sptrains.spikechannels;
+    else
+        %make sure we are using valid channels
+        group2 = intersect(group2,sptrains.spikechannels);
+    end
+    if nargin < 7
+        dosave = 1;
+    end
+    if nargin < 6
+        doplot = 1;
+    end
+    if nargin < 5
+        sort_event = 'target';
+    end
+    if ischar(logfile)
+        logfile = fopen(logfile,'w');
+    end
+    %get the number of cells
+    ncells = 0;
+    ncells1 = 0;
+    ncells2 = 0;
+    ncombos = 0;
+    %report the number of comparison before starting the loop
+    ncells1 = sum(sptrains.unitsperchannel(ismember(sptrains.spikechannels,group1)));
+    if all(group1==group2)
+        ncells2 = ncells1;
+        ncombos = nchoosek(ncells1,2)-ncells1;
+    else
+        ncells2 = sum(sptrains.unitsperchannel(ismember(sptrains.spikechannels,group2)));
+        ncombos = ncells1*ncells2;
+    end
+    fprintf(logfile,'Computing %d combinations...\n', ncombos);
+    
+    nbins = length(bins);
+    Z = zeros(ncells1,ncells2,nbins);
+    Zs = zeros(ncells1,ncells2,nbins);
+    Zi = zeros(ncells1,ncells2,nbins);
+	Ii = zeros(ncells1+ncells2,nbins); %independent
+    k1 = 1; %keeps track of cells
+    onsets = [];
+    offsets = [];
+    comboidx = [];
+    k = 1;
+	for ch1=1:length(group1)
+		clusters = sptrains.channels(group1(ch1)).cluster;
+		for j1=1:length(clusters)
+            k2 = 1;
+			
+			for ch2=1:length(group2)
+				clusters2 = sptrains.channels(group2(ch2)).cluster;
+                for j2=1:length(clusters2)
+                    %skip if both clusters are the same
+                    if (ch1 == ch2) && (j1==j2)
+                        k2 = k2 + 1; %make sure we still increaes the counter
+                        k = k + 1;
+                        continue
+                        
+                    end
+                    dfname = sprintf('g%.2dc%.2dsg%.2dc%.2ds%sJointInformation.mat', sptrains.spikechannels(ch1),j1,...
+                            sptrains.spikechannels(ch2),j2,sort_event);
+                    %check if we already have the data computed
+                    trains = {clusters(j1) clusters2(j2)};
+                    if exist(dfname,'file')
+                        load(dfname);
+                        Ii(k1,:) = H1-Hc1;
+                        Ii(k2,:) = H2-Hc2;
+                        if exist('Hs','var')
+                            if all((mean(Hs-Hcs,1))'>H-Hc)
+                                fprintf(1,'Recomputing shuffled..\n');
+                                clear Hs Hcs
+                            end
+                        end
+                        if ~exist('Hs','var')
+                            if ~exist('counts','var')
+                                [counts,bins] = getJointTrialCounts(trains,trials,bins,'alignment_event',alignment_event);
+                                counts = permute(counts,[2,3,1]);
+                            end
+                            [Hs,Hcs,bins] = computeJointInformation(counts,bins,trials,1,alignment_event);
+                        end
+                    else
+                   		if all(Z(k2,k1,:) == 0)
+							
+							%get the counts
+							[counts,bins] = getJointTrialCounts(trains,trials,bins,'alignment_event',alignment_event);
+							counts = permute(counts,[2,3,1]);
+                            %paired information
+							[H,Hc,bins,bias,Hi,Hic] = computeJointInformation(counts,bins,trials,0,alignment_event);
+							%shuffled pair information
+							[Hs,Hcs,bins] = computeJointInformation(counts,bins,trials,1,alignment_event);
+							%independent
+							
+                            [H1,Hc1,bins] = computeInformation(squeeze(counts(:,:,1)),bins,trials);
+                            Ii(k1,:) = H1-Hc1;
+                            [H2,Hc2,bins] = computeInformation(squeeze(counts(:,:,2)),bins,trials);
+                            Ii(k2,:) = H2-Hc2;
+                            
+							%save results
+							
+                           
+                        
+						end
+                    end
+                    if dosave
+                        disp(['Saving result to file ' dfname '...']);
+                        save(dfname,'H','Hc','Hi','Hic','Hs','Hcs','H1','Hc1','H2','Hc2','bins','counts');
+                    end
+                    [onset,offset] = getSignificantInterval(H-Hc,Hs-Hcs,bins);
+                    if ~isnan(onset)
+                        fprintf(logfile,'g%.2dc%.2dsg%.2dc%.2ds\t%f\t%f\n', sptrains.spikechannels(ch2),j2,...
+									sptrains.spikechannels(ch1),j1,onset,offset);
+                    end
+                    comboidx = [comboidx; k*ones(length(onset),1)];
+                    onsets = [onsets;onset];
+                    offsets = [offsets;offset];
+                    
+                    Z(k1,k2,:) = H-Hc;
+					Z(k2,k1,:) = Z(k1,k2,:);
+                    Zs(k1,k2,:) = mean(Hi-Hic,1) + 3*std(Hi-Hic);
+					Zs(k2,k1,:) = Zs(k1,k2,:);
+                    Zi(k1,k2,:) = Ii(k1,:) + Ii(k2,:);
+					Zi(k2,k1,:) = Zi(k1,k2,:);
+
+                    if doplot
+                        plotLocationInformation(squeeze(Z(k1,k2,:)),bins,alignment_event,trials,'I_shuffled',Hs-Hcs,'I_ind',Hi-Hic);
+                        fname = sprintf('g%.2dc%.2dsg%.2dc%.2ds%sJointInformation.pdf', sptrains.spikechannels(ch1),j1,...
+                            sptrains.spikechannels(ch2),j2,sort_event);
+                        print('-dpdf',fname);
+                        close
+                    end
+                    k2 = k2 + 1;
+                    k = k+1;
+                end
+                
+            end
+            k1 = k1 + 1;
+        end
+        
+    end
+    %print out a table of
+	fprintf(logfile,'Number of combos with at least one significant interval: %d\n', length(unique(comboidx(~isnan(onsets)))));
+	fprintf(logfile,'Median onset: %f\n', nanmedian(onsets));
+	fprintf(logfile,'Median offset: %f\n', nanmedian(offsets));
+	fprintf(logfile,'Median interval length: %f\n', nanmedian(offsets-onsets));
+    if logfile > 1
+        fclose(logfile)
+    end
+end
---
+++ computeJointInformation.m
@@ -0,0 +1,140 @@
+function [H,Hc,bins,bias,Hi,Hci] = computeJointInformation(counts,bins,trials,shuffle,sort_event)
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	%Compute the information contained in the counts matrix.
+	%Input:
+	%	counts		:		[ntrials X nbins-1 X ncells]		:		matrix of spike counts
+	%	bins		:		[nbins,1]				:		the bins use to compute the spike counts
+	%	trials		:		structure array containing information about the trials used
+	%	shuffle		:		whether we should also compute shuffle information. Defaults to 0 (no)
+	%	sort_event	:		the event used to sort the trials. This defaults to 'target'
+	%Output:
+	%	H			:		Total entropy for each time bin
+	%	Hc			:		Conditional entropy for each time bin
+	%	bins		:		bins used to compute the spike counts
+	%	bias		:		the Panzeri-Treves bias correction factor for the information
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	if nargin < 5
+		sort_event = 'target';
+	end
+	if nargin < 4
+		shuffle = 0;
+	end
+	ntrials = size(counts,1);
+	trial_labels = zeros(ntrials,1);
+	rows = zeros(ntrials,1);
+	columns = zeros(ntrials,1);
+	%get the row and column of the target
+	for t=1:length(trials)
+		e = getfield(trials(t),sort_event);
+        if strcmpi(sort_event,'distractors')
+            rows(t) = e(2);
+            columns(t) = e(3);
+        else
+            rows(t) = e.row;
+            columns(t) = e.column;
+        end
+	end
+	nrows = max(rows);
+	ncols = max(columns);
+	trial_labels = (rows-1).*ncols + columns;
+
+	nbins = length(bins);
+	%get the unique labels
+	u = unique(trial_labels);
+	ncond = length(u);
+	%get the cardinality of the counts
+	uc = unique(counts);
+	mx = length(uc);
+	if ~shuffle
+        if nargout == 6
+            [H,Hc,bias,Hi,Hci] = computeEntropies(counts,uc,trial_labels,u,bins);
+        else
+            [H,Hc,bias] = computeEntropies(counts,uc,trial_labels,u,bins);
+        end
+	else
+		H = zeros(100,nbins);
+		Hc = zeros(100,nbins);
+		bias = zeros(100,nbins);
+		for k=1:100
+			tl = randsample(trial_labels,length(trial_labels));
+			[H(k,:),Hc(k,:),bias(k,:)] = computeEntropies(counts,uc,tl,u,bins);
+		end
+	end
+
+	function [H,Hc,bias,Hi,Hci] = computeEntropies(counts,unique_counts,trial_labels,unique_labels,bins)
+		word_size = size(counts,3);
+		ncond = length(unique_labels);
+		uc = unique_counts;
+		u = unique_labels;
+		ntrials = length(trial_labels);
+		mxc = max(max(max(counts)))+1;
+		cconv = mxc.^(0:word_size-1);
+		N = mxc^word_size; %maximum word hash
+		hash_bins = [0:N-1];
+        pC = zeros(nbins,ncond,N); %conditional
+        pCi = zeros(100,nbins,ncond,N); %independent conditional
+        pi = zeros(100,nbins,N,1); %independent unconditional
+		p = zeros(nbins,N,1); %unconditional
+		H = zeros(nbins,1);
+        Hi = zeros(100,nbins,1);
+		Hc = zeros(nbins,1);
+        Hci = zeros(100,nbins,1);
+		Rs = zeros(nbins,ncond);
+		R = zeros(nbins,1);
+		bias = zeros(nbins,1);
+		for j=1:nbins
+			for i=1:ncond
+				tidx = trial_labels == u(i);
+				%get 'word_size' bins from the current bin and hash them
+				c = squeeze(counts(tidx,j,:));
+                w = c*cconv';
+				%count the instances
+				n = histc(w,hash_bins);
+                
+				%normalize
+				pC(j,i,:) = n/sum(n);
+				Rs(j,i) = sum(n>0);
+                if nargout ==5
+                    %create a shuffled version
+                    cs = zeros(size(c));
+                    for ss=1:100
+                        for l=1:size(c,2)
+                            cs(:,l) = c(randsample(1:size(c,1),size(c,1)),l);
+                        end
+                         ws = cs*cconv';
+                         ns = histc(ws,hash_bins);
+                         pCi(ss,j,i,:) = ns/sum(ns);
+                    end  
+                end
+			end
+			%construct total probabilities
+            w = squeeze(counts(:,j,:))*cconv';
+            c = squeeze(counts(:,j,:));
+			p(j,:) = histc(w,hash_bins);
+            
+			R(j) = sum(p(j,:)>0);
+			p(j,:) = p(j,:)/sum(p(j,:));
+            if nargout == 5
+                cs = zeros(size(counts,1),size(counts,3));
+                for ss=1:100
+                    for l=1:size(c,2)
+                        cs(:,l) = c(randsample(1:size(c,1),size(c,1)),l);
+                    end
+                     ws = cs*cconv';
+                     ns = histc(ws,hash_bins);
+                     pi(ss,j,:) = ns/sum(ns);
+                end 
+            end
+			H(j) = -p(j,:)*log2(p(j,:) + (p(j,:)==0))';
+            Hi(:,j) = -sum(pi(:,j,:).*log2(pi(:,j,:) + (pi(:,j,:)==0)),3);
+			hc = -sum(pC(j,:,:).*log2(pC(j,:,:)+(pC(j,:,:)==0)),3);
+			hci = -sum(pCi(:,j,:,:).*log2(pCi(:,j,:,:)+(pCi(:,j,:,:)==0)),4);
+            ns = histc(trial_labels,u);
+			Hc(j) = (ns/sum(ns))'*hc'; 
+            Hci(:,j) = sum(repmat((ns/sum(ns))',[100,1]).*squeeze(hci),2); 
+			%pt bias correction
+			%get the number of observerd responses
+			bias(j) = 1/(2*ntrials*log(2))*(sum(Rs(j,:)-1,2) - R(j)+1);
+		end
+	end
+end
