982b0198d7dbea105f426f4836e6cf5e390b476c
Roger Herikstad <roger.herikstad@gmail.com>
2013-10-03 14:58:11 +0800

    Added computation of bin-shuffled information when compute temporal information

--- computeTemporalInformation.m
+++ computeTemporalInformation.m
@@ -1,4 +1,4 @@
-function [H,Hc,bins,bias] = computeTemporalInformation(counts,bins,word_size,trials,shuffle,sort_event)
+function [H,Hc,bins,bias,Hi,Hci] = computeTemporalInformation(counts,bins,word_size,trials,shuffle,sort_event)
 	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	%Compute the information contained in the counts matrix.
 	%Input:
@@ -41,24 +41,30 @@ function [H,Hc,bins,bias] = computeTemporalInformation(counts,bins,word_size,tri
 
 	nbins = length(bins);
 	%get the unique labels
-	[u,k,j] = unique(trial_labels);
+	[u,kk,jj] = unique(trial_labels);
 	ncond = length(u);
 	%get the cardinality of the counts
 	uc = unique(counts);
 	mx = length(uc);
 	if ~shuffle
-		[H,Hc,bias] = computeEntropies(counts,uc,trial_labels,u,bins);
+        if nargout == 6
+            [H,Hc,bias,Hi,Hci] = computeEntropies(counts,uc,trial_labels,u,bins);
+        else
+            [H,Hc,bias] = computeEntropies(counts,uc,trial_labels,u,bins);
+        end
 	else
 		H = zeros(100,nbins);
 		Hc = zeros(100,nbins);
 		bias = zeros(100,nbins);
+        kku = trial_labels(kk);
 		for k=1:100
-			tl = randsample(trial_labels,length(trial_labels));
+            us = randsample(u,length(u));
+			tl = us(jj);
 			[H(k,:),Hc(k,:),bias(k,:)] = computeEntropies(counts,uc,tl,u,bins);
 		end
 	end
 
-	function [H,Hc,bias] = computeEntropies(counts,unique_counts,trial_labels,unique_labels,bins)
+	function [H,Hc,bias,Hi,Hci] = computeEntropies(counts,unique_counts,trial_labels,unique_labels,bins)
 		ncond = length(unique_labels);
 		uc = unique_counts;
 		mx = length(uc);
@@ -69,9 +75,13 @@ function [H,Hc,bins,bias] = computeTemporalInformation(counts,bins,word_size,tri
 		N = sum(cconv)+1; %maximum word hash
 		hash_bins = [0:N-1];
         pC = zeros(nbins,ncond,N); %conditional
+        pCi = zeros(100,nbins,ncond,N); %independent conditional
 		p = zeros(nbins,N,1); %unconditional
+        pi = zeros(100,nbins,N,1); %independent unconditional
 		H = zeros(nbins,1);
+        Hi = zeros(100,nbins,1);
 		Hc = zeros(nbins,1);
+        Hci = zeros(100,nbins,1);
 		Rs = zeros(nbins,ncond);
 		R = zeros(nbins,1);
 		bias = zeros(nbins,1);
@@ -86,17 +96,45 @@ function [H,Hc,bins,bias] = computeTemporalInformation(counts,bins,word_size,tri
 				%normalize
 				pC(j,i,:) = n/sum(n);
 				Rs(j,i) = sum(n>0);
+                if nargout ==5
+                    %create a shuffled version
+                    cs = zeros(size(c));
+                    for ss=1:100
+                        for l=1:size(c,2)
+                            cs(:,l) = c(randsample(1:size(c,1),size(c,1)),l);
+                        end
+                         ws = cs*cconv';
+                         ns = histc(ws,hash_bins);
+                         pCi(ss,j,i,:) = ns/sum(ns);
+                    end  
+                end
 			end
 			%construct total probabilities
             w = counts(:,j:j+word_size-1)*cconv';
 			p(j,:) = histc(w,hash_bins);
 			R(j) = sum(p(j,:)>0);
 			p(j,:) = p(j,:)/sum(p(j,:));
-
+            if nargout == 5
+                cs = zeros(size(counts,1),word_size);
+                for ss=1:100
+                    for l=1:size(c,2)
+                        cs(:,l) = counts(randsample(1:size(cs,1),size(cs,1)),j+l-1);
+                    end
+                     ws = cs*cconv';
+                     ns = histc(ws,hash_bins);
+                     pi(ss,j,:) = ns/sum(ns);
+                end 
+            end
 			H(j) = -p(j,:)*log2(p(j,:) + (p(j,:)==0))';
+            
 			hc = -sum(pC(j,:,:).*log2(pC(j,:,:)+(pC(j,:,:)==0)),3);
+            hci = -sum(pCi(:,j,:,:).*log2(pCi(:,j,:,:)+(pCi(:,j,:,:)==0)),4);
 			ns = histc(trial_labels,u);
-			Hc(j) = (ns/sum(ns))'*hc'; 
+            Hc(j) = (ns/sum(ns))'*hc';
+            if nargout == 5
+                Hi(:,j) = -sum(pi(:,j,:).*log2(pi(:,j,:) + (pi(:,j,:)==0)),3);
+                Hci(:,j) = sum(repmat((ns/sum(ns))',[100,1]).*squeeze(hci),2); 
+            end
 			%pt bias correction
 			%get the number of observerd responses
 			bias(j) = 1/(2*ntrials*log(2))*(sum(Rs(j,:)-1,2) - R(j)+1);
