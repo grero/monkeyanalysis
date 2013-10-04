25260c7431f911d7a677c6f93fac51cf787918a7
Roger Herikstad <roger.herikstad@gmail.com>
2013-10-04 11:56:37 +0800

    Added function to return spike trains for specific channels

---
+++ getAreaSpiketrain.m
@@ -0,0 +1,21 @@
+function out_sptrains = getAreaSpiketrain(sptrains, channels)
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	%Get the spike trains for specific channels
+	%Input:
+	%	sptrains	:		a structure containing all the spike trains
+	%	channels	:		the chnanels for which we want to return spike trains
+	%Output:
+	%	out_sptrains	:	the spike trains from the specified channels
+	ntrains = 0;
+	out_sptrains.ntrains = ntrains;
+	%get those of the requested channels with spikesh
+	cchannels = intersect(channels,sptrains.spikechannels);
+	if ~isempty(cchannels) 
+		for ch = 1:length(cchannels)
+			out_sptrains.channels(cchannels(ch)).cluster = sptrains.channels(cchannels(ch)).cluster;
+			ntrains = ntrains + 1;
+		end
+	end
+	out_sptrains.spikechannels = cchannels;
+	out_sptrains.ntrains = ntrains;
+end
