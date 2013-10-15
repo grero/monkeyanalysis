21b33c79d00ce0a83217fb0db014fd3d07613b84
Roger Herikstad <roger.herikstad@gmail.com>
2013-10-15 11:20:07 +0800

    Added getEventTimingDistr from devel

---
+++ getEventTimingDistr.m
@@ -0,0 +1,43 @@
+function [response_mean, response_std] = getEventTimingDistr(trials,event,alignment_event)
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %Return the distribution of timinigs for the specified event
+    %Input:
+    %   trials          :   array structure with trial information
+    %   event           :   name of event to get the distribution of
+    %   alignment_event :   event to which to align the timings
+    %Output:
+    %   response_mean   :   mean timing of the specified event
+    %   response_std    :   standard deviation of timing of the specified
+    %                       event
+    if ~isfield(trials,event)
+        disp('Invalid event')
+        return
+    end
+    if nargin < 3
+        alignment_event = 'target';
+    end
+    if ~isfield(trials,alignment_event)
+        disp('Invalid alignment event')
+        return
+    end
+    response = zeros(length(trials),1);
+    for t=1:length(trials)
+        r = getfield(trials(t),event);
+        if isempty(r)
+            continue;
+        end
+        if isstruct(r)
+            response(t) = r.timestamp;
+        else
+            response(t) = r;
+        end
+        alignto = getfield(trials(t),alignment_event);
+        if isstruct(alignto)
+            alignto = alignto.timestamp;
+        end
+        response(t) = (response(t)-alignto)*1000;
+    end
+    response = response(response>0);
+    response_mean = mean(response);
+    response_std = std(response);
+end
