56683005f5ae26503d0fbcbc47a5d1a092552854
Roger Herikstad <roger.herikstad@gmail.com>
2013-11-24 10:36:30 +0800

    Added documentation for getPopulationRaster and plotPopulationRaster

--- getPopulationRaster.m
+++ getPopulationRaster.m
@@ -1,4 +1,18 @@
 function [aligned_sptrains, trial_idx, cellidx] = getPopulationRaster(sptrains,trials,window)
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	%Return population raster aligned to target onset
+	%Input:
+	%	sptrains	:	structure containing spike time information for all units
+	%	trials		:	structure containing info about the trials
+	%	window		:	[min,max] tuple with the analysis window limits. E.g. (-200,1700) will
+	%					return spike for all cells falling between -200ms before target onset and 1700ms
+	%					after target onset
+	%Output:
+	%	aligned_sptrains	:	flat array containing spikes falling within the specified window for all cells
+	%							and all trials
+	%	trial_idx			:	the trial to which each spike belongs
+	%	cellidx				:	the cell to which eac spike belongs
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	aligned_sptrains = [];
 	trial_idx = [];
 	cellidx = [];
--- plotPopulationRaster.m
+++ plotPopulationRaster.m
@@ -1,4 +1,15 @@
 function plotPopulationRaster(aligned_spikes,trial_idx,cell_idx, trials_to_plot)
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	%Plot the population raster returned by getPopulationRaster
+	%Input:
+	%	aligned_spikes	:	flat array with all spikes from all cells and all trials
+	%	trial_idx		:	the trial index of ecah spike
+	%	cell_idx		:	the unit from which each spike was elicited
+	%	trials_to_plot	:	array of trials to plot
+	%Example:
+	% 	plotPopulationRaster(aligned_sptrains,trial_idx,cellidx,find(trial_labels==17))
+	%		plots all spikes from all cells for trials at position 17
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	%get the indices of the trials to plot
 	idx = find(ismember(trial_idx,trials_to_plot));
 	trials = trial_idx(idx);
