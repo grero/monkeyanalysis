dfccd4f55b9484d0ae2be1cf411f40f3c632e1b6
Roger Herikstad <roger.herikstad@gmail.com>
2014-01-07 17:45:42 +0800

    Added code for decoding population counts

---
+++ decode.m
@@ -0,0 +1,57 @@
+function class_errs = decode(counts,trial_labels,runs,type,trial_shuffle)
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	%Decode the trial labels using joint counts
+	%Input:
+	%	counts	[ncells X ntrials X nbins ]		:	spike counts for multiple cells over multiple bins
+	%	trial_labels	[ntrials]				:	label for each trial, used to group the trials
+	%	runs									:	number of re-sampling runs
+	%	type									:	the type of decoding algorithm to use. Defaults to 'linear', which is a linear discriminant with pooled covariance matrix.
+	%												See 'help classify' for other possible decoding algorithms
+	%	trial_shuffle							:	indicate whether to shuffle trials within condition in the training phase. This breaks up any trial-by-trial relationship between cells and results in a decoder that 
+	%												is insensitive to the correlation structure between cells
+	%Output:
+	%	class_errs	[ncats X nbins X nruns]		:	the relative number of errors made by the decoder for each category in each run
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	%
+	if nargin < 5
+		trial_shuffle = 0;
+	end
+    if nargin < 4
+        type = 'linear';
+    end
+    n = length(trial_labels);
+    nbins = size(counts,3);
+    err = zeros(nbins,runs);
+    ntrain = floor(0.8*n); %number of training samples
+    ntest = n-ntrain; %number of test samples
+    class = nan*zeros(ntest,nbins,runs); %test result
+    class_errs = nan*zeros(length(unique(trial_labels)),nbins,runs); %errors per class
+    ul = unique(trial_labels); %unique trial labels
+    for k=1:runs
+        idx = randsample(1:n,ntrain); %grab a random set of trial for training
+        tidx = setdiff(1:n,idx); %grab the remaining trials for testing
+        for i=1:nbins
+            try
+                if ~trial_shuffle
+                    class(:,i,k) = classify(squeeze(counts(:,tidx,i))',squeeze(counts(:,idx,i))',trial_labels(idx),type);
+                else
+					%shuffle the training sample; note that since we are shuffling within conditions, the labels remain unchanged
+					tcounts = shuffleTrials(counts(:,idx,i),trial_labels(idx));
+                    class(:,i,k) = classify(squeeze(counts(:,tidx,i))',tcounts',trial_labels(idx),type);
+                end
+                for j=1:size(class_errs,1)
+                    qidx = trial_labels(tidx)==ul(j);
+                    class_errs(j,i,k) = sum(class(qidx,i,k)~=ul(j))./sum(qidx);
+                end
+            catch e
+                e;
+            end
+        end
+        err(:,k) = sum(squeeze(class(:,:,k))==repmat(trial_labels(tidx),1,nbins))/ntest;
+    end
+end
+    
+
+    
+    
+    
