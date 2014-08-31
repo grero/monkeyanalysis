7cf13f26d28ac09681656e1cf2c9264fe025b937
Roger Herikstad <roger.herikstad@gmail.com>
2014-08-31 10:57:24 +0800

    If the spiketrains file contains sptrains segreated into areas, these will be loaded in separated structures

--- loadSpiketrains.m
+++ loadSpiketrains.m
@@ -19,25 +19,52 @@ function sptrains = loadSpiketrains(fname)
 		%load the first file
 		sptrains_ = load(files(1).name);
 	else
-		sptrains_ = load(fname);	
+		if ischar(fname)
+			sptrains_ = load(fname);	
+		elseif isstruct(fname)
+			sptrains_ = fname;
+		end
 	end
 	F = fields(sptrains_);
 	sptrains = struct;
     spikechannels = [];
-    
+  	area_sptrains = struct();
 	for i=1:length(F)
 		f = F(i);
-		q = sscanf(f{:}, 'g%dc%ds');
+		sidx = strfind(f{:}, '_');
+		if isempty(sidx)
+			q = sscanf(f{:}, 'g%dc%ds');
+			sptrains.channels(q(1)).cluster(q(2)).spiketimes = getfield(sptrains_,f{:}); 
+		else
+			ss = f{:};
+			area_name = ss(1:sidx(1)-1);
+			spname = ss(sidx(1)+1:end);
+			q = sscanf(ss(sidx(1)+1:end), 'g%dc%ds');
+			if isfield(area_sptrains,area_name)
+				qq = setfield(getfield(area_sptrains,area_name),spname,getfield(sptrains_,f{:}));
+				area_sptrains = setfield(area_sptrains,area_name,qq);
+			else
+				area_sptrains = setfield(area_sptrains,area_name,struct(spname,getfield(sptrains_,f{:})));
+			end
+		end
         spikechannels = [spikechannels q(1)]; 
-		sptrains.channels(q(1)).cluster(q(2)).spiketimes = getfield(sptrains_,f{:}); 
     end
-    sptrains.spikechannels = unique(spikechannels);
-    sptrains.ntrains = length(F);
-    unitsperchannel = zeros(1,length(sptrains.spikechannels));
-    for i=1:length(sptrains.spikechannels)
-        unitsperchannel(i) = length(sptrains.channels(sptrains.spikechannels(i)).cluster);
-    end
-    sptrains.unitsperchannel = unitsperchannel;
+	if ~isempty(fieldnames(area_sptrains))
+		sptrains = struct()
+		F = fieldnames(area_sptrains)
+		for i=1:length(F)
+			f = F(i);
+			sptrains = setfield(sptrains,f{:},loadSpiketrains(getfield(area_sptrains,f{:})));
+		end
+	else
+		sptrains.spikechannels = unique(spikechannels);
+		sptrains.ntrains = length(F);
+		unitsperchannel = zeros(1,length(sptrains.spikechannels));
+		for i=1:length(sptrains.spikechannels)
+			unitsperchannel(i) = length(sptrains.channels(sptrains.spikechannels(i)).cluster);
+		end
+		sptrains.unitsperchannel = unitsperchannel;
+	end
 end
 
 
