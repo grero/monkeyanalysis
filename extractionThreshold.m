0bfb6cf4b2ec399b4d2f9248a4638e6139e04746
Roger Herikstad <roger.herikstad@gmail.com>
2015-08-16 10:00:23 +0800

    Fixed white space errors

--- extractionThreshold.m
+++ extractionThreshold.m
@@ -2,15 +2,16 @@ function [sumX,sumX2,n] = extractionThreshold(data,neurongroup_info,threshold,gr
 %ExtractorWrapper	Computes mean and stdev used during spike extraction
 %	[sumX sumX2 n]=ExtractorWrapper(DATA,
 %	DESCRIPTOR,GROUP_INFO) computes the mean and and 4 standard
-%	deviations for each group in DATA and then recomputes these two 
-%	variables after clipping out points that are larger than 4 
-%	standard deviations.  DESCRIPTOR is the data structure 
-%	returned by ReadDescriptor which contains descriptions of the data. 
+%	deviations for each group in DATA and then recomputes these two
+%	variables after clipping out points that are larger than 4
+%	standard deviations.  DESCRIPTOR is the data structure
+%	returned by ReadDescriptor which contains descriptions of the data.
 %	GROUP_INFO is the data structure returned by GroupSignals which
-%	describes how groups of data are organized. The sum and sum of 
+%	describes how groups of data are organized. The sum and sum of
 %   squared amplitudes is returned for the clipped data.  Also the number
-%   of points in the clipped data.  Since only a portion of the 
-%   entire session data is usually inputed, these parameters are used to calculate
+%   of points in the clipped data.  Since only a portion of the
+%   entire session data is usually inputed,
+these parameters are used to calculate
 %   the mean and standard deviation of the entire data set.
 
 % Calculate 4-sigma thresholds for each channel
@@ -22,31 +23,31 @@ end
 if nargin<4
     groups=1:size(neurongroup_info,2);
 end
-% Loops over group number  
+% Loops over group number
 fprintf([' Group ' num2str(groups)])
 for i=groups
     group=neurongroup_info(i).group;
     groupdata=[];
     %...we only want to give data for one group at a time
-    groupdata=[data(neurongroup_info(i).channels,:)];	
-    
+    groupdata=[data(neurongroup_info(i).channels,:)];
+
     %c=cleandata(groupdata,1);
     %groupdata = transpose(c(:,1:size(groupdata,1)));
-    
+
     clip_data=groupdata';
     sigma=std(clip_data);
     avg=mean(clip_data);
     % comptute positive threshold
-    v_plusthreshold= avg + threshold*sigma; 
+    v_plusthreshold= avg + threshold*sigma;
     avgplus=mean(v_plusthreshold);    %threshold for all channels in group
     % compute negative threshold
     v_minusthreshold = avg - threshold*sigma;
     avgminus=mean(v_minusthreshold);   %threshold for all channels in group
-    % chigh=abs(clip_data)>avg2;	
+    % chigh=abs(clip_data)>avg2;
     % clow=abs(clip_data)<avg2;
     % meanavg=ones(size(chigh,1),1)*avg;
-    % clip_data=clip_data .* clow + meanavg .* chigh; 
-    
+    % clip_data=clip_data .* clow + meanavg .* chigh;
+
     % set points larger than positive threshold to 1 and the rest 0
     cliphigh=clip_data>avgplus;
     % set points smaller than negative threshold to 1 and the rest 0
@@ -59,26 +60,26 @@ for i=groups
     %    % unclippedlow=invert(cliplow);
     %    % pull out the data that is between +/- threshold and replace all points
     %    % exceeding the threshold with the mean value
-    %    
+    %
     %    clip_data=(unclipped .* clip_data) + (outliers * diag(avg));
-    
+
     %figure;plot(clip_data(:,1));hold on
     %collapse accross channels
-    
-    
-    
+
+
+
     %outliers = sum(outliers,2);
     %clip_data(find(outliers),:)=[];
     clip_data(outliers>=1)=NaN;
     %plot(clip_data(:,1),'r');
-    
+
     sumX(i,:) = nansum(clip_data,1);
     sumX2(i,:) = nansum(clip_data.^2,1);
     %n(i,:) = size(clip_data,1);
     n(i,:) = sum(~isnan(clip_data),1);
-    
+
     %     v_mean(i,:) = mean(clip_data);
-    %     v_threshold(i,:)=threshold*std(clip_data);   
-    
+    %     v_threshold(i,:)=threshold*std(clip_data);
+
 end
 
