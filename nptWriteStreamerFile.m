3035aec8094e0cef76adf14f1bc301baa5af8749
Roger <roger@work10.neuralc>
2015-10-27 10:34:56 +0800

    Added support for writing transposed nptData

--- nptWriteStreamerFile.m
+++ nptWriteStreamerFile.m
@@ -1,16 +1,24 @@
-function status=nptWriteStreamerFile(filename,sampling_rate,data,scan_order)
+function status=nptWriteStreamerFile(filename,sampling_rate,data,transpose,scan_order)
 %nptWriteStreamerFile Function to write binary files from Data Streamer
 %function status=nptWriteStreamerFile(filename,sampling_rate,data,scan_order)
 %scanorder is optional
 
 num_channels=size(data,1);
-header_size=74;
+if nargin < 4
+	transpose = 0;
+end
+
+if transpose == 1
+	header_size=75;
+else
+	header_size=74;
+end
 
 fid=fopen(filename,'w','ieee-le');
 if fid~=-1
    status=1;
 end
-if nargin==3
+if nargin<5
    scan_order=zeros(1,64);
    pad=[];
 else
@@ -20,8 +28,15 @@ end
 
 fwrite(fid, header_size, 'int32');					% 4 bytes reserved for header size which is 73 bytes
 fwrite(fid,num_channels, 'uint16');				% 2 bytes
+if transpose == 1
+	fwrite(fid,transpose, 'int8');
+end
 fwrite(fid,sampling_rate, 'uint32');				% 4 bytes
 fwrite(fid,scan_order,'uchar');				% 1 byte for each channel up to 64 channels
 fwrite(fid,pad,'int8');
-fwrite(fid, data, 'int16');
+if transpose
+	fwrite(fid, data', 'int16');
+else
+	fwrite(fid, data, 'int16');
+end
 fclose(fid);
